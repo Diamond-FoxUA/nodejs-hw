9298cf351db038b83ae46d14247a49ea2bcd4ff7
Dmytro Farbun <dimofarb@gmail.com>
2026-01-07 22:21:30 +0200

    feat: add notes and noteId routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
+// import pino from 'pino';
+// import cors from 'cors';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello, world" });
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: "Retrived all notes" });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const noteId = req.params.noteId;
+  res.status(200).json({ message: `Retrived note with ID: ${noteId}` });
 });
 
 app.listen(PORT, () => {
