2e578d901e054ae849fce1cb885c3fd8f5298646
Dmytro Farbun <dimofarb@gmail.com>
2026-01-07 22:36:04 +0200

    feat: add cors, pino, express.json

--- src/server.js
+++ src/server.js
@@ -1,9 +1,27 @@
 import express from 'express';
-// import pino from 'pino';
-// import cors from 'cors';
+import pino from 'pino-http';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 const PORT = process.env.PORT ?? 3000;
 
