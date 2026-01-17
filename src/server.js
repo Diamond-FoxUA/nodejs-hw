f5d5125268f5beef0cfa9356ee100dba2c9ef1ca
Dmytro Farbun <dimofarb@gmail.com>
2026-01-17 20:49:30 +0200

    fix(server): align imports and middleware usage with specification

--- src/server.js
+++ src/server.js
@@ -3,17 +3,17 @@ import cors from 'cors';
 import helmet from "helmet";
 import 'dotenv/config';
 import { logger } from './middleware/logger.js';
-import connectMongoDB from './db/connectMongoDB.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 
-app.use(express.json({limit: "5mb"}));
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(logger());
+app.use(logger);
 
 const PORT = process.env.PORT ?? 3000;
 
