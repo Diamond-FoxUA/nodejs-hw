7e407daffbf3bfbcd629b80987c113c653c2b8b0
Dmytro Farbun <dimofarb@gmail.com>
2026-01-17 20:28:31 +0200

    refactor: extract pino to the logger.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import pino from 'pino-http';
 import cors from 'cors';
 import helmet from "helmet";
 import 'dotenv/config';
+import logger from './middleware/logger.js';
 import connectMongoDB from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
@@ -13,21 +13,7 @@ const app = express();
 app.use(express.json({limit: "5mb"}));
 app.use(cors());
 app.use(helmet());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+app.use(logger());
 
 const PORT = process.env.PORT ?? 3000;
 
