76230ca88f40212b1911a832183e3df9d00083cd
Dmytro Farbun <dimofarb@gmail.com>
2026-01-24 21:16:16 +0200

    feat(errors): add error handling middleware from celebrate

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { logger } from './middleware/logger.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { errors } from 'celebrate';
 import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
@@ -20,7 +21,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
-
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
