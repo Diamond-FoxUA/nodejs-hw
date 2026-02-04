0ededd70fbdb5910d6993c94e496c8f94537b8e7
Dmytro Farbun <dimofarb@gmail.com>
2026-02-05 00:25:29 +0200

    feat(notes): restrict notes operations to current user and update Note model

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,7 +5,8 @@ export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
   const skip = (page - 1) * perPage;
 
-  const noteQuery = Note.find();
+  const noteQuery = Note.find({ userId: req.user._id });
+
   if (tag) {
     noteQuery.where("tag").equals(tag);
   }
@@ -35,7 +36,10 @@ export const getAllNotes = async (req, res) => {
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
 
-  const note = await Note.findById(noteId);
+  const note = await Note.findOne({
+      userId: req.user._id,
+      noteId
+   });
   if (!note) {
     throw createHttpError(404, "Note not found");
   }
@@ -44,7 +48,10 @@ export const getNoteById = async (req, res) => {
 };
 
 export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
+  const note = await Note.create({
+    ...req.body,
+    userId: req.user._id
+  });
 
   res.status(201).json(note);
 };
@@ -53,7 +60,8 @@ export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
 
   const note = await Note.findOneAndDelete({
-    _id: noteId
+    _id: noteId,
+    userId: req.user._id
   });
   if (!note) {
     throw createHttpError(404, "Note not found");
@@ -65,7 +73,7 @@ export const deleteNote = async (req, res) => {
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findOneAndUpdate(
-    { _id: noteId },
+    { _id: noteId, userId: req.user._id },
     req.body,
     { new: true }
   );
