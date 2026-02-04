1556d6ab61cba672d56ebbf17f7736418f2b82b1
Dmytro Farbun <dimofarb@gmail.com>
2026-02-04 23:01:26 +0200

    fix(auth): correct validation schema for password in registerUserSchema

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/authController.js";
 import { registerUserSchema } from "../validations/authValidation.js";
+import { celebrate } from "celebrate";
 
 const router = Router();
 
-router.post('/auth/register', registerUserSchema, registerUser);
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
 
 export default router;
