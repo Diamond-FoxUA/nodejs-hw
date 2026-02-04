915620e7a8017112ee08401fa34580b20bfbf79a
Dmytro Farbun <dimofarb@gmail.com>
2026-02-04 23:32:13 +0200

    feat(auth): implement user login with validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { registerUser, logoutUser } from "../controllers/authController.js";
-import { registerUserSchema } from "../validations/authValidation.js";
+import { registerUser, logoutUser, loginUser } from "../controllers/authController.js";
+import { registerUserSchema, loginUserSchema } from "../validations/authValidation.js";
 import { celebrate } from "celebrate";
 
 const router = Router();
 
 router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 router.post('/auth/logout', logoutUser);
 
 export default router;
